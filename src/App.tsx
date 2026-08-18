650ac7d11b86bf55bb936e9d021bea6fe3ac2062
Narayan Singh <narayan.10838244@ltimindtree.com>
2026-08-18 20:15:56 +0530

    implemented inc, dec and reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number>(0);
+  const [warning, setWarning] = useState<boolean>(false)
 
+  const increaseCount = () => {
+      setCount(prev => prev + 1)
+      setWarning(false);
+  }
+
+  const decreaseCount = () => {
+      if(count > 0) setCount(prev => prev - 1)
+      else setWarning(true);
+  }
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+    <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "567px"}}>
+      <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", border: "0.5px solid black", margin: "100px", width: "100%", height: "400px", borderRadius: "4px"}}>
+        <div style={{display: "flex", justifyContent: "center", alignItems: "center", border: "0.5px dashed black", borderRadius: "4px", width: "80px", marginBottom: "30px"}}>
+          <h2>{count}</h2>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
+        <div style={{display: "fixed", gap: "10px", marginBottom: "20px"}}>
+          <button onClick={decreaseCount} style={{border: "none", padding: "4px", background: "red", color: "white", borderRadius: "4px", width: "80px"}}>-</button>
+          <button onClick={increaseCount} style={{border: "none", padding: "4px", background: "green", color: "white", borderRadius: "4px", width: "80px"}}>+</button>
+          <button onClick={() => setCount(0)} style={{border: "none", padding: "4px", background: "yellow", color: "black", borderRadius: "4px", width: "80px"}}>Reset</button>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+        {warning && 
+        <div style={{border: "0.1px solid red", color: "red", borderRadius: "4px", padding: "4px"}}>
+          Please note that value can not be less then 0!!!
+        </div>}
+      </div>
+    </div>
   )
 }
 
